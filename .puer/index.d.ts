6b13859664ce2fabb7dbeadbba7cef11ce386307
wellshsu <wellshsu@github.com>
2025-05-12 19:24:52 +0800

    fix: 修改 puer 适配器的类型声明 index.d.ts 文件

--- .puer/index.d.ts
+++ .puer/index.d.ts
@@ -2,27 +2,27 @@ declare namespace CS {
     namespace EFramework.Modulize {
         namespace XModule {
             /**
-             * 事件绑定器
-             * @param eid 事件标识
-             * @param module 模块类型
-             * @param once 回调一次
+             * 事件标记特性，使用此特性可以为事件定义参数。
+             * @param eid 事件标识。
+             * @param module 模块类型。
+             * @param once 回调一次。
              */
             export function Event(eid: number, module: CS.System.Type = null, once: boolean = false): (target: any, propertyKey: string) => void
         }
 
         namespace XView {
             /**
-             * 模块绑定器
-             * @param type 模块类型
+             * 视图模块特性，用于绑定模块。
+             * @param type 模块类型。
              */
             export function Module(type: CS.System.Type): (target: any) => void
 
             /**
-             * 组件绑定器
-             * @param name 组件名称
-             * @param method 函数名称
+             * 视图元素特性，支持自定义描述。
+             * @param name 元素名称。
+             * @param method 自定义描述。
              */
-            export function Element(name: string, method: string = null): (target: any, propertyKey: string) => void
+            export function Element(name: string, extras: string = null): (target: any, propertyKey: string) => void
         }
     }
 }
